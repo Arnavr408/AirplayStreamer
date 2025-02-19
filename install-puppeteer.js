21bf5e31af519de931077ba4d9f82e11f92f06f6
Arnavr408 <115211302+Arnavr408@users.noreply.github.com>
2025-02-19 17:27:40 +0400

    Delete install-puppeteer.js

--- install-puppeteer.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-    console.log("📦 Installing Puppeteer Chrome...");
-    await puppeteer.createBrowserFetcher().download('latest');
-    console.log("✅ Puppeteer installed successfully!");
-})();
